ee906478ab899120fcaac0d125c03b012261ba98
erjondd <erjon.dinarama@gmail.com>
2025-07-16 18:53:01 +0200

    Dogs page

--- src/pages/Dogs/Dogs.jsx
+++ src/pages/Dogs/Dogs.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "./index.module.scss";
+import Container from "../../components/Container/Container";
 
 function Dogs() {
   return (
@@ -6,9 +8,19 @@ function Dogs() {
       <Container>
         <section className={styles.hero}>
           {/* <div className={styles.overlay}></div> */}
-          <h1>Our Dogs</h1>
-          <p>
-          </p>
+          <h1>Discover our Doberman collection</h1>
+          <p>Home / Our Dogs</p>
+          <div className={styles.filters}>
+            <div className={styles.fourty}>Race</div>
+            <div className={styles.twenty}>Age</div>
+            <div className={styles.twenty}>Size</div>
+            <div className={styles.twenty}>Color</div>
+            <div className={styles.twenty}>Gender </div>
+            <div className={styles.twenty}>Location</div>
+            <div className={styles.twenty}>Cert</div>
+            <div className={styles.twenty}>Vaccinated</div>
+            <div className={styles.twenty}>Price Range</div>
+          </div>
         </section>
       </Container>
     </section>
